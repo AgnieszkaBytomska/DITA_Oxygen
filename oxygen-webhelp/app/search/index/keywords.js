a8cd15936c506bd664c952016b374ba2ac12b28d
Agnieszka <a.bytomska@gmail.com>
2021-03-14 19:30:35 +0100

    x

---
+++ oxygen-webhelp/app/search/index/keywords.js
@@ -0,0 +1,24 @@
+define(function() {var keywords=[{w:"Document",p:["p0"]},{w:"Purpose",p:["p0"]},{w:"Design",p:["p1","p6"]},{w:"Review",p:["p1"]},{w:"Timeline",p:["p1"]},{w:"Email",p:["p2"]},{w:"Reminders",p:["p2"]},{w:"and",p:["p2"]},{w:"Notifications",p:["p2"]},{w:"Overview",p:["p3","p4","p6"]},{w:"of",p:["p4"]},{w:"Deployment",p:["p4"]},{w:"Submitting",p:["p5"]},{w:"a",p:["p5","p9","p10"]},{w:"Travel",p:["p5","p7","p11","p12"]},{w:"Request",p:["p5","p9","p10"]},{w:"System",p:["p6"]},{w:"Technical",p:["p6"]},{w:"Updating",p:["p7","p12"]},{w:"Your",p:["p7"]},{w:"Profile",p:["p7"]},{w:"Access",p:["p8"]},{w:"Cancel",p:["p9"]},{w:"Recall",p:["p10"]},{w:"Arranger",p:["p11"]},{w:"Preferences",p:["p12"]},{w:"My",p:["p13","p14","p15","p16"]},{w:"first",p:["p13","p15","p16"]},{w:"concept",p:["p13","p14"]},{w:"second",p:["p14"]},{w:"reference",p:["p15"]},{w:"topic",p:["p15"]},{w:"task",p:["p16"]}];
+var ph={};
+ph["p0"]=[0, 1];
+ph["p1"]=[2, 3, 4];
+ph["p2"]=[5, 6, 7, 5, 8];
+ph["p3"]=[9];
+ph["p4"]=[9, 10, 11];
+ph["p5"]=[12, 13, 14, 15];
+ph["p6"]=[16, 2, 17, 9];
+ph["p7"]=[18, 19, 14, 20];
+ph["p8"]=[21];
+ph["p9"]=[22, 13, 15];
+ph["p10"]=[23, 13, 15];
+ph["p12"]=[18, 14, 25];
+ph["p11"]=[14, 24];
+ph["p14"]=[26, 29, 28];
+ph["p13"]=[26, 27, 28];
+ph["p16"]=[26, 27, 32];
+ph["p15"]=[26, 27, 30, 31];
+     return {
+         keywords: keywords,
+         ph: ph
+     }
+});
---
+++ oxygen-webhelp/app/search/index/link-to-parent.js
@@ -0,0 +1,4 @@
+/*Maps current topic to its parent: "topicIndex:parentIndex". -1 represents the map.*/
+define(function () {
+return {13:7,8:7,4:3,0:3,12:7,2:-1,6:3,5:-1,9:5,3:-1,1:3,11:5,7:-1,10:5};
+});
